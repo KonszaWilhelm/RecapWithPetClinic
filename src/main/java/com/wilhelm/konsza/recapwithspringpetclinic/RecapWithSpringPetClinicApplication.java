ce81376813d058f2e234ef95238deae87a997e97
KonszaWilhelm <44321955+KonszaWilhelm@users.noreply.github.com>
2021-04-27 15:36:53 +0300

    Initial Project ADD

---
+++ src/main/java/com/wilhelm/konsza/recapwithspringpetclinic/RecapWithSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.wilhelm.konsza.recapwithspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapWithSpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecapWithSpringPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wilhelm/konsza/recapwithspringpetclinic/RecapWithSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilhelm.konsza.recapwithspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapWithSpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
